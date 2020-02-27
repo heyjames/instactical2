8c4c25d92413b7b5b7c17423937361739712b5aa
PC\James <heyjameslam@gmail.com>
2020-02-27 01:57:38 -0800

    Implement Guidelines API(GET, POST)

--- src/components/guidelines.jsx
+++ src/components/guidelines.jsx
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { getGuidelines } from '../services/fakeGuidelines';
+import Banner from './banner';
+import parse from 'html-react-parser';
 
-const Guidelines = () => {
-  const guidelines = getGuidelines().content;
-  const jumbotronStyle = {
-    backgroundColor: "#426397",
-    padding: "2rem 1rem"
-  };
-  return (
-    <div>
-      <div className="jumbotron jumbotron-fluid" style={jumbotronStyle}>
-        <div className="container text-light">
-          <h2 className="display-6">Guidelines</h2>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-xl">
-            <p>{guidelines}</p>
+class Guidelines extends Component {
+  state = { _id: "", title: "", content: "" }
+
+  async componentDidMount() {
+    const { data } = await getGuidelines();
+    const { _id, title, content } = data[0];
+
+    this.setState({ _id, title, content });
+  }
+
+  render() {
+    const jumbotronStyle = {
+      backgroundColor: "#426397",
+      padding: "2rem 1rem"
+    };
+
+    return (
+      <div>
+        <div className="jumbotron jumbotron-fluid" style={jumbotronStyle}>
+          <div className="container text-light">
+            <h2 className="display-6">Guidelines</h2>
           </div>
         </div>
-      </div>
-    </div>
-  );
+        <div className="container">
+          <div className="row">
+            <div className="col-xl">
+              <p>{this.state.title}</p>
+              {parse(this.state.content)}
+            </div>
+          </div>
+        </div>
+      </div>);
+  }
 }
 
 export default Guidelines;
--- src/services/fakeGuidelines.js
+++ src/services/fakeGuidelines.js
@@ -1,9 +1,18 @@
+import axios from 'axios';
+const apiUrl = "http://localhost:3001/api/guidelines";
+
 const guidelines =
   {
     _id: "1",
     content: "Objectives / Caches"
   };
 
+// Simulate timer
+async function sleep(sec) {
+  return new Promise(resolve => { setTimeout(() => resolve(), sec * 1000) });
+}
+
 export function getGuidelines() {
-  return guidelines;
+  // await sleep(2);
+  return axios.get(apiUrl);
 }
