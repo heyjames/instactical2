9c590d86db42e68337e35f67f96f424176d38e67
James <>
2020-07-19 19:43:20 -0700

    Fix Row empty prop bug

--- src/components/common/row.jsx
+++ src/components/common/row.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 const Row = ({ children, addToRowClass, customColClass }) => {
   const defaultColClass = "col-md-8 offset-md-2";
   customColClass = (customColClass) ? customColClass : defaultColClass;
+  addToRowClass = (addToRowClass) ? " " + addToRowClass : "";
 
   return (
-    <div className={"row " + addToRowClass}>
+    <div className={"row" + addToRowClass}>
       <div className={customColClass}>
         {children}
       </div>
