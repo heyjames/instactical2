d68bd36ca284a0fe88c1233dc4793a2ef3c54f29
James <>
2020-08-27 00:40:19 -0700

    Add loading spinner and fix memory leak. Refactor.

--- src/components/announcementForm.jsx
+++ src/components/announcementForm.jsx
@@ -4,47 +4,58 @@ import Joi from 'joi-browser';
 import Form from './form';
 import Input from './input';
 import TextArea from './textArea';
+import moment from 'moment';
 import {
   getAnnouncement,
   deleteAnnouncement,
   createAnnouncement,
   saveAnnouncement
 } from '../services/announcementService';
-import moment from 'moment';
+import { pause } from './common/utils';
+import { renderLoadingIndicator } from './common/loading';
+import Row from './common/row';
+import Container from './common/container';
 
 class AnnouncementForm extends Component {
-  state = {
-    data: { announcement: "", createdAt: "", updatedAt: "" },
-    errors: {},
-    pageTitle: { title: "" },
-    formState: ""
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      data: {
+        announcement: "",
+        createdAt: "",
+        updatedAt: ""
+      },
+      errors: {}
+    }
+  
+    this._isMounted = false;
   }
 
-  schema = {
-    _id: Joi.string(),
-    announcement: Joi.string()
-      .min(1)
-      .required()
-      .label('Announcement')
-      .trim()
-  };
-
-  async componentDidMount() {
-    this.renderBannerTitle();
-    await this.populateAnnouncement();
-    this.setFormState();
-  }
-
-  setFormState() {
-    let formState = "edit";
-
+  componentDidMount() {
+    this._isMounted = true;
     const announcementId = this.props.match.params.id;
-    if (announcementId === "new") formState = "create";
-
-    return this.setState({ formState });
+    
+    (announcementId === "new")
+      ? this.stopLoading()
+      : this.populateAnnouncement(announcementId);
   }
 
-  mapToViewModel(announcement) {
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  // schema = {
+  //   _id: Joi.string(),
+  //   announcement: Joi.string()
+  //     .min(1)
+  //     .required()
+  //     .label('Announcement')
+  //     .trim()
+  // };
+
+  mapToViewModel = announcement => {
     return {
       _id: announcement._id,
       announcement: announcement.content,
@@ -53,46 +64,82 @@ class AnnouncementForm extends Component {
     }
   }
 
-  async populateAnnouncement() {
-    const announcementId = this.props.match.params.id;
-    if (announcementId === "new") return;
+  stopLoading = async () => {
+    try {
+      await pause(0.2);
+    } catch (ex) {
+      this.props.history.replace("/");
+    }
 
-    const announcement = await getAnnouncement(announcementId);
-    if (!announcement) return this.props.history.replace("/not-found");
+    if (this._isMounted) {
+      return this.setState({ loading: false });
+    }
+  }
 
-    this.setState({ data: this.mapToViewModel(announcement) });
+  populateAnnouncement = async announcementId => {
+    try {
+      await pause(0.35);
+      const announcement = await getAnnouncement(announcementId);
+      if (!announcement) return this.props.history.replace("/not-found");
+      const data = this.mapToViewModel(announcement)
+      const loading = false;
+  
+      if (this._isMounted) {
+        this.setState({ data, loading });
+      }
+    } catch (ex) {
+      if (ex.response.status === 403) {
+        this.props.history.replace("/unauthorized");
+      } else {
+        this.props.history.replace("/");
+      }
+    }
   }
 
   handleCreate = async () => {
     const { announcement } = this.state.data;
-    await createAnnouncement({ content: announcement });
-    this.props.history.push("/announcements");
+
+    try {
+      await createAnnouncement({ content: announcement });
+      this.props.history.push("/announcements");
+    } catch (ex) {
+      this.props.history.replace("/");
+    }
   }
 
   handleDelete = async announcementId => {
     const confirmMsg = "Are you sure?";
+
     if (window.confirm(confirmMsg)) {
-      await deleteAnnouncement(announcementId);
-      this.props.history.push("/announcements");
+      try {
+        await deleteAnnouncement(announcementId);
+        this.props.history.push("/announcements");
+      } catch (ex) {
+        this.props.history.replace("/");
+      }
     }
   }
 
-  handleSave = async () => {
+  handleSave = async announcementId => {
     const { data } = this.state;
-    const { id } = this.props.match.params;
 
-    await saveAnnouncement({
-      _id: id,
-      content: data.announcement,
-      updatedAt: data.updatedAt
-    });
-
-    this.props.history.push("/announcements");
+    try {
+      await saveAnnouncement({
+        _id: announcementId,
+        content: data.announcement,
+        updatedAt: data.updatedAt
+      });
+  
+      this.props.history.push("/announcements");
+    } catch (ex) {
+      this.props.history.replace("/");
+    }
   }
 
   handleChange = ({ currentTarget: input }) => {
     const data = { ...this.state.data }
     data.announcement = input.value;
+    
     this.setState({ data });
   }
 
@@ -100,17 +147,9 @@ class AnnouncementForm extends Component {
     this.props.history.push("/announcements");
   }
 
-  renderBannerTitle() {
-    let pageTitle = { title: "Edit Announcement" };
-    const announcementId = this.props.match.params.id;
-    if (announcementId === "new") pageTitle.title = "Create a new announcement";
-    this.setState({ pageTitle });
-  }
-
-  renderTextArea = () => {
+  renderTextArea = announcement => {
     return (
       <div className="form-group">
-        <label htmlFor="announcement">Announcement</label>
         <textarea
           autoFocus
           className="form-control"
@@ -118,73 +157,108 @@ class AnnouncementForm extends Component {
           id="announcement"
           rows="4"
           onChange={this.handleChange}
-          value={this.state.data.announcement}
+          value={announcement}
         >
         </textarea>
       </div>
     )
   }
 
-  renderBtns = () => {
-    const { formState } = this.state;
-    const { id } = this.props.match.params;
+  renderCreateBtns = announcementId => {
+    if (announcementId !== "new") return;
+    
+    return (
+      <button
+        className="btn btn-primary mr-2"
+        onClick={() => this.handleCreate()}
+      >
+        Create
+      </button>
+    )
+  }
 
-    if (formState === "create") {
-      return (
-        <React.Fragment>
-          <button
-            className="btn btn-secondary mr-2"
-            onClick={() => this.handleCancel()}>
-            Cancel</button>
-          <button
-            className="btn btn-primary mr-2"
-            onClick={() => this.handleCreate()}>
-            Create</button>
-        </React.Fragment>
-      )
-    }
+  renderEditBtns = announcementId => {
+    if (announcementId === "new") return;
+    
+    return (
+      <React.Fragment>
+        <button
+          className="btn btn-danger"
+          onClick={() => this.handleDelete(announcementId)}
+        >
+          Delete
+        </button>
+        <button
+          className="btn btn-success ml-2"
+          onClick={() => this.handleSave(announcementId)}
+        >
+          Save
+        </button>
+      </React.Fragment>
+    )
+  }
 
-    if (formState === "edit") {
-      return (
-        <React.Fragment>
-          <button
-            className="btn btn-secondary mr-2"
-            onClick={() => this.handleCancel()}>
-            Cancel</button>
-          <button
-            className="btn btn-danger"
-            onClick={() => this.handleDelete(id)}>
-            Delete</button>
-          <button
-            className="btn btn-success ml-2"
-            onClick={() => this.handleSave()}>
-            Save</button>
-        </React.Fragment>
-      )
-    }
+  renderBtns = announcementId => {
+    return (
+      <React.Fragment>
+        <button
+          className="btn btn-secondary mr-2"
+          onClick={() => this.handleCancel()}
+        >
+          Cancel
+        </button>
+        {this.renderCreateBtns(announcementId)}
+        {this.renderEditBtns(announcementId)}
+      </React.Fragment>
+    )
+  }
+
+  getPageStyles = () => {
+    const pageStyles = {};
+
+    pageStyles.bannerStyle = {
+      backgroundColor: "#424242",
+      padding: "2rem 1rem",
+      marginBottom: "0"
+    };
+
+    pageStyles.backgroundStyle = {
+      backgroundColor: "#f5f5f5",
+      padding: "2rem 1rem",
+      marginBottom: "0"
+    };
+
+    return pageStyles;
+  }
+
+  getBannerInfo = announcementId => {
+    const pageTitle = { title: "" };
+
+    pageTitle.title = (announcementId === "new")
+      ? "Create A New Announcement"
+      : "Edit Announcement";
+
+    return pageTitle;
   }
 
   render() {
-    const jumbotronStyle = {
-      backgroundColor: "#424242",
-      padding: "2rem 1rem"
-    };
-
-    const { pageTitle } = this.state;
+    const { data: { announcement }, loading } = this.state;
+    const { bannerStyle, backgroundStyle } = this.getPageStyles();
+    const announcementId = this.props.match.params.id;
+    const bannerInfo = this.getBannerInfo(announcementId);
 
     return (
       <React.Fragment>
-        <Banner info={pageTitle} style={jumbotronStyle} />
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4 offset-md-4">
-              <form onSubmit={(e) => e.preventDefault()}>
-                {this.renderTextArea()}
-                {this.renderBtns()}
-              </form>
-            </div>
-          </div>
-        </div>
+        <Banner info={bannerInfo} style={bannerStyle} />
+        <Container style={backgroundStyle}>
+          {loading
+            ? renderLoadingIndicator()
+            : <Row customColClass="col-md-6 offset-md-3">
+                {this.renderTextArea(announcement)}
+                {this.renderBtns(announcementId)}
+              </Row>
+          }
+        </Container>
       </React.Fragment>
     );
   }
--- src/components/announcements.jsx
+++ src/components/announcements.jsx
@@ -2,87 +2,120 @@ import React, { Component } from 'react';
 import Banner from './banner';
 import { Link } from 'react-router-dom';
 import { getAnnouncements } from '../services/announcementService';
-import axios from 'axios';
-import moment from 'moment';
 import Pagination from './pagination';
 import { paginate } from '../utils/paginate';
 import Time from './time';
+import { pause } from './common/utils';
+import { renderLoadingIndicator } from './common/loading';
+import Container from './common/container';
 
 class Announcements extends Component {
   state = {
+    loading: true,
     announcements: [],
     currentPage: 1,
     pageSize: 2
   };
+  
+  _isMounted = false;
 
   async componentDidMount() {
+    this._isMounted = true;
     document.title = "Announcements - insTactical";
-    let { data } = await getAnnouncements();
-    data = data.sort((a, b) => (a._id < b._id) ? 1 : -1);
 
-    this.setState({ announcements: data });
+    try {
+      await pause(0.35);
+      let { data } = await getAnnouncements();
+      data = data.sort((a, b) => (a._id < b._id) ? 1 : -1);
+      const loading = false;
+
+      if (this._isMounted) {
+        this.setState({ announcements: data, loading });
+      }
+    } catch (ex) {
+      if (ex.response.status === 403) {
+        this.props.history.replace("/unauthorized");
+      } else {
+        this.props.history.replace("/");
+      }
+    }
   }
 
-  handlePageChange = (page) => { this.setState({ currentPage: page }); }
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
 
-  render() {
-    const pageTitle = { title: "Announcements" };
-    const jumbotronStyle = {
+  handlePageChange = page => { this.setState({ currentPage: page }); }
+
+  initializePageStyles = () => {
+    const pageStyles = {};
+
+    pageStyles.bannerStyle = {
       backgroundColor: "#424242",
       padding: "2rem 1rem",
       marginBottom: "0"
     };
-    const { announcements: allAnnouncements, currentPage, pageSize } = this.state;
-    const { length: count } = this.state.announcements;
 
+    pageStyles.backgroundStyle = {
+      backgroundColor: "#f5f5f5",
+      marginBottom: "0"
+    };
+
+    return pageStyles;
+  }
+
+  render() {
+    const bannerInfo = { title: "Announcements" };
+    const { announcements: allAnnouncements, currentPage, pageSize, loading } = this.state;
+    const { length: count } = this.state.announcements;
     const announcements = paginate(allAnnouncements, currentPage, pageSize);
     const { user } = this.props;
+    const { bannerStyle, backgroundStyle } = this.initializePageStyles();
 
     return (
       <React.Fragment>
-        <Banner info={pageTitle} style={jumbotronStyle} />
-        <div className="jumbotron jumbotron-fluid" style={{ backgroundColor: "#f5f5f5", marginBottom: "0" }}>
-          <div className="container">
+        <Banner info={bannerInfo} style={bannerStyle} />
+        <Container style={backgroundStyle}>
+          {(loading)
+            ? renderLoadingIndicator()
+            : <React.Fragment>
+                <div className="row">
+                  <div className="col-md-4">
+                    <Pagination
+                      itemsCount={count}
+                      currentPage={this.state.currentPage}
+                      pageSize={this.state.pageSize}
+                      onPageChange={this.handlePageChange}
+                    />
+                  </div>
 
-            <div className="row">
-              <div className="col-md-4">
-                <Pagination
-                  itemsCount={count}
-                  currentPage={this.state.currentPage}
-                  pageSize={this.state.pageSize}
-                  onPageChange={this.handlePageChange}
-                />
-              </div>
+                  <div className="col-md-6">
+                    {(user && user.isAdmin) && <div className="pb-4">
+                      <Link to={"/announcements/new/"}>
+                        <button
+                          className="btn btn-sm btn-primary">
+                          <i className="fa fa-plus" aria-hidden="true"></i> New</button>
+                      </Link>
+                    </div>}
 
-              <div className="col-md-6">
-
-                {(user && user.isAdmin) && <div className="pb-4">
-                  <Link to={"/announcements/new/"}>
-                    <button
-                      className="btn btn-sm btn-primary">
-                      <i className="fa fa-plus" aria-hidden="true"></i> New</button>
-                  </Link>
-                </div>}
-
-
-                <div className="card shadow-sm rounded">
-                  <ul className="list-group list-group-flush">
-                    {announcements.map(announcement =>
-                      <li key={announcement._id} className="list-group-item">
-                        <div>{announcement.content}</div>
-                        <div className="small text-muted">
-                          <Time data={announcement} />
-                        </div>
-                        {(user && user.isAdmin) && <Link to={"/announcements/" + announcement._id}>Edit</Link>}
-                      </li>
-                    )}
-                  </ul>
+                    <div className="card shadow-sm rounded">
+                      <ul className="list-group list-group-flush">
+                        {announcements.map(announcement =>
+                          <li key={announcement._id} className="list-group-item">
+                            <div>{announcement.content}</div>
+                            <div className="small text-muted">
+                              <Time data={announcement} />
+                            </div>
+                            {(user && user.isAdmin) && <Link to={"/announcements/" + announcement._id}>Edit</Link>}
+                          </li>
+                        )}
+                      </ul>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
+              </React.Fragment>
+          }
+        </Container>
       </React.Fragment>
     );
   }
